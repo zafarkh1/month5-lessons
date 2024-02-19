e58c1e4471a659571e913ce25e70435d426df2c7
zafarkh <hatamovzafar7070@gmail.com>
2024-02-19 19:07:04 +0500

    'minar_changes'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
-import ComponentA from './component/ComponentA'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./component/Header";
+import Sidebar from "./component/Sidebar";
+import HomePage from "./component/pages/HomePage";
+import VideoPage from "./pages/VideoPage";
+import SearchResultsPage from "./pages/SearchResultsPage.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ComponentA />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <div className="container">
+          <Sidebar />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/watch/:videoId" component={VideoPage} />
+            <Route path="/results/:searchQuery" component={SearchResultsPage} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
